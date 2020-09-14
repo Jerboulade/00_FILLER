d38ec16556d2e2982bfc06d2facd5f2943cc3d2e
Jerome Carere <jer-car@hotmail.com>
2020-09-14 12:45:28 +0200

    global scope .h OK

--- filler/includes/filler.h
+++ filler/includes/filler.h
@@ -13,7 +13,6 @@
 #ifndef FILLER_H
 # define FILLER_H
 # include "../libft/libft.h"
-# include "stdio.h"
 # include <math.h>
 
 typedef struct	s_map
@@ -42,7 +41,7 @@ typedef struct	s_player
 {
 	char		player;
 	int			n_blok;
-	int 		*blok;
+	int			*blok;
 }				t_player;
 
 typedef struct	s_opponent
@@ -58,7 +57,6 @@ typedef struct	s_game
 	t_piece		pc;
 	t_player	pl;
 	t_opponent	op;
-	int			fd_bot; // norme pour bot.txt?
 	int			lap;
 	char		*line;
 	int			top_i;
@@ -79,53 +77,53 @@ typedef struct	s_place
 /*
 **		printgame.c
 */
-void	print_game(t_game *game, char *option);
+void			print_game(t_game *game, char *option);
 /*
 **		filler.c
 */
-void	reset(t_game *game, int end);
+void			reset(t_game *game, int end);
 
 /*
 **		init_game.c
 */
-void	init_map(t_game *game);
-void	init_piece(t_game *game);
-void	init_player(t_game *game);
-void	init_opponent(t_game *game);
-void	init_game(t_game *game);
+void			init_map(t_game *game);
+void			init_piece(t_game *game);
+void			init_player(t_game *game);
+void			init_opponent(t_game *game);
+void			init_game(t_game *game);
 /*
 **		get_info.c
 */
-int		side(t_game *game);
-int		get_piece(t_game *game);
-int		create_map(t_game *game);
-int		get_map(t_game *game, int i, int j);
-int		get_players(t_game *game);
+int				side(t_game *game);
+int				get_piece(t_game *game);
+int				create_map(t_game *game);
+int				get_map(t_game *game, int i, int j);
+int				get_players(t_game *game);
 /*
 **		parsing.c
 */
-int		parse_piece(t_game *game);
-int		compare_map(t_game *game);
-int		parse_map(t_game *game, int i, int j);
+int				parse_piece(t_game *game);
+int				compare_map(t_game *game);
+int				parse_map(t_game *game, int i, int j);
 /*
 **		get_placement.c
 */
-int		*dup_blok(int *blok, int n_blok);
-int		check_if_out(t_game *g, t_place *p, int i);
-int		index_translation(t_game *g, t_place *p, int i);
-void	adapt_index(t_game *game, t_place *p);
-int		get_placement(t_game *game, int *start, int n_start, int i);
+int				*dup_blok(int *blok, int n_blok);
+int				check_if_out(t_game *g, t_place *p, int i);
+int				index_translation(t_game *g, t_place *p, int i);
+void			adapt_index(t_game *game, t_place *p);
+int				get_placement(t_game *game, int *start, int n_start, int i);
 /*
 **		put_piece.c
 */
-int		absolute_distance(t_game *g, int i, int j, int k);
-int		check_surround(t_game *game, int p, int c, int nb);
-void	put_piece(t_game *game);
-int		algo(t_game *game, int i, int j, int k);
+int				absolute_distance(t_game *g, int i, int j, int k);
+int				check_surround(t_game *game, int p, int c, int nb);
+void			put_piece(t_game *game);
+int				algo(t_game *game, int i, int j, int k);
 /*
 **		tool.c
 */
-int		*ft_intab(size_t size);
-void	ft_intdel(int **as);
-void	ft_frintab(int **tab, int size);
+int				*ft_intab(size_t size);
+void			ft_intdel(int **as);
+void			ft_frintab(int **tab, int size);
 #endif
